2f44e6cf34657d6d2c24d56a22de755c096f6b1d
Anil Thanneru <56641929+Anil0612@users.noreply.github.com>
2019-10-29 17:57:12 +0530

    Update track.py

--- track.py
+++ track.py
@@ -1,10 +1,11 @@
 
 import numpy as np
 import cv2 as cv
-img = cv.imread('Solarsystem.png')
+img = cv.imread('Googleimage.png')
+#img = cv.imread('Solarsystem.png')
 print(img.shape)
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-blur=cv.GaussianBlur(img,(15,15),0)
+blur=cv.GaussianBlur(img,(17,17),0)
 #ret, thresh = cv.threshold(blur, 127, 255, 0)
 ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY | cv.THRESH_OTSU)
 #th2 = cv.adaptiveThreshold(blur,255,cv.ADAPTIVE_THRESH_MEAN_C,\
@@ -26,7 +27,7 @@ for cnt in contours:
     perimeter = cv.arcLength(cnt,True)
 
     if (perimeter>150):
-        cv.drawContours(img,[cnt],-1,(0,255,0),2)
+        cv.drawContours(img,[cnt],0,(0,255,0),2)
         cv.imshow('RGB',img)
         cv.waitKey(1000)
         print(len(cnt))
