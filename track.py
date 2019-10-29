90a6be3739d922567e4656a80de12b0396f62d76
Anil Thanneru <56641929+Anil0612@users.noreply.github.com>
2019-10-29 19:19:46 +0530

    Update track.py

--- track.py
+++ track.py
@@ -1,25 +1,19 @@
 
 import numpy as np
 import cv2 as cv
-img = cv.imread('Googleimage.png')
-#img = cv.imread('Solarsystem.png')
+#img = cv.imread('Googleimage.png')
+img = cv.imread('Solarsystem.png')
+#img = cv.imread('salesforcelogo.png')
 print(img.shape)
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-blur=cv.GaussianBlur(img,(17,17),0)
+blur=cv.GaussianBlur(img,(15,15),0)
 #ret, thresh = cv.threshold(blur, 127, 255, 0)
-ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY | cv.THRESH_OTSU)
+ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY| cv.THRESH_OTSU)
 #th2 = cv.adaptiveThreshold(blur,255,cv.ADAPTIVE_THRESH_MEAN_C,\
  #        cv.THRESH_BINARY_INV,11,1)
-#contours, hierarchy = cv.FindContours_Impl(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 (contours,hierachy)=cv.findContours(th3,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
 
 cv.drawContours(blur, contours, -1, (0,255,0), 3)
-#mask = np.zeros(imgray.shape,np.uint8)
-#cv.drawContours(mask,[cnt],0,255,-1)
-#pixelpoints = np.transpose(np.nonzero(mask))
-#cnt = contours[4]
-#cv.drawContours(img, [cnt], 0, (0,255,0), 3)
-#cv.drawContours(img, contours, 3, (0,255,0), 3)
 c=0
 for cnt in contours:
 
